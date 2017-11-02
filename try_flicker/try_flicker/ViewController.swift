ef11b8733cfc39e883b64d3ece54a83e0c880f7f
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-11-02 12:41:26 -0700

    init project

---
+++ try_flicker/try_flicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  try_flicker
+//
+//  Created by Wataru Maeda on 2017/11/02.
+//  Copyright © 2017 Wataru Maeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
