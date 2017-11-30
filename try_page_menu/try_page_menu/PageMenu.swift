f45b7df5e8826858da9a0ec2918a9edf95998519
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-11-29 18:35:19 -0800

    Added page menu base view controller class

---
+++ try_page_menu/try_page_menu/PageMenu.swift
@@ -0,0 +1,29 @@
+//
+//  PageMenu.swift
+//  try_page_menu
+//
+//  Created by Wataru Maeda on 2017/11/29.
+//  Copyright © 2017 com.wataru.maeda. All rights reserved.
+//
+
+import UIKit
+
+class PageMenu: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
+// MARK: - Menu Items (ScrollView)
+
+extension PageMenu {
+  
+}
+
+
+// MARK: - Item (CollectionView)
+
+extension PageMenu {
+  
+}
