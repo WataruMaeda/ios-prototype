6f8e17a5854a721ac830016d8a3ad17c93f16f47
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-12-12 21:30:28 -0800

    Update sample

--- try_twitter_login/try_twitter_login/ViewController.swift
+++ try_twitter_login/try_twitter_login/ViewController.swift
@@ -12,6 +12,19 @@ import TwitterKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var profileImageView: UIImageView! {
+    didSet {
+      profileImageView.clipsToBounds = true
+      profileImageView.contentMode = .scaleAspectFill
+      profileImageView.layer.cornerRadius = profileImageView.frame.size.width / 2
+    }
+  }
+  @IBOutlet var userNameLabel: UILabel! {
+    didSet {
+      userNameLabel.isUserInteractionEnabled = false
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     initTwitterLoginButton()
@@ -20,12 +33,51 @@ class ViewController: UIViewController {
   func initTwitterLoginButton() {
     let logInButton = TWTRLogInButton(logInCompletion: { session, error in
       if (session != nil) {
-        print("signed in as \(String(describing: session?.userName))");
+        
+        print("[userID]: \(String(describing: session?.userID))");
+        print("[userName]: \(String(describing: session?.userName))");
+        print("[authToken]: \(String(describing: session?.authToken))");
+        print("[authTokenSecret]: \(String(describing: session?.authTokenSecret))");
+        
+        // Show User Name
+        if let userName = session?.userName {
+          self.userNameLabel.text = userName
+          self.userNameLabel.textColor = .black
+        }
+        
+        // Show Profile Image
+        if let userId = session?.userID {
+          let twitterClient = TWTRAPIClient(userID: userId)
+          twitterClient.loadUser(withID: userId, completion: { (user, error) in
+            guard let profileUrl = user?.profileImageURL else { return }
+            print("[profileImageURL]: \(profileUrl))");
+            self.setProfileImage(stringUrl: profileUrl)
+          })
+        }
+        
       } else {
         print("error: \(String(describing: error?.localizedDescription))");
       }
     })
-    logInButton.center = self.view.center
-    self.view.addSubview(logInButton)
+    let centerY = userNameLabel.frame.maxY + logInButton.frame.size.height / 2 + 40
+    logInButton.center = CGPoint(x: view.frame.size.width / 2, y: centerY)
+    view.addSubview(logInButton)
+  }
+  
+  func setProfileImage(stringUrl: String) {
+    guard let url = URL(string: stringUrl) else { return }
+    getDataFromUrl(url: url) { data, response, error in
+      guard let data = data, error == nil else { return }
+      print(response?.suggestedFilename ?? url.lastPathComponent)
+      DispatchQueue.main.async() {
+        self.profileImageView.image = UIImage(data: data)
+      }
+    }
+  }
+  
+  func getDataFromUrl(url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      completion(data, response, error)
+      }.resume()
   }
 }
