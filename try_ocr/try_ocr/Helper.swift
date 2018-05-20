0c7deb329abb2cae4d1f6fa179080f31c2219b44
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-05-20 14:04:08 -0700

    chore: add drawer

---
+++ try_ocr/try_ocr/Helper.swift
@@ -0,0 +1,81 @@
+//
+//  Helper.swift
+//  try_ocr
+//
+//  Created by Wataru Maeda on 2018-05-20.
+//  Copyright © 2018 com.watarumaeda. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - UIVew+Ext
+
+extension UIView {
+  
+  var viewWidth: CGFloat {
+    return frame.size.width
+  }
+  
+  var viewHeight: CGFloat {
+    return frame.size.height
+  }
+}
+
+// MARK: - UIButton+Ext
+
+extension UIButton {
+  
+  static func generateButton(title: String, frame: CGRect, callback: @escaping () -> Void) -> UIButtonCallback {
+    let button = UIButtonCallback()
+    button.frame = frame
+    button.setTitle(title, for: .normal)
+    button.titleLabel?.font = UIFont.systemFont(ofSize: 24)
+    button.setTitleColor(.darkGray, for: .normal)
+    button.handleTap(event: .touchUpInside) { _ in
+      callback()
+    }
+    return button
+  }
+}
+
+// MARK: - UIImage+Ext
+
+extension UIImage {
+  
+  static func convertToImage(view: UIView) -> UIImage? {
+    UIGraphicsBeginImageContext(view.frame.size)
+    view.layer.render(in: UIGraphicsGetCurrentContext()!)
+    let img = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    guard let image = img else { return nil }
+    guard let imageData = UIImagePNGRepresentation(image) else { return nil }
+    return UIImage(data: imageData)
+  }
+}
+
+// MARK: - UIAlert+Ext
+
+extension UIAlertController {
+  
+  static func show(_ viewController:  UIViewController, title: String) {
+    let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+    viewController.present(alert, animated: true, completion: nil)
+  }
+}
+
+// MARK: - Callback Button
+
+class UIButtonCallback: UIButton {
+  
+  private lazy var callback: (UIButton)->Void = { button in }
+  
+  func handleTap(event: UIControlEvents, callback: @escaping (UIButton)->Void) {
+    self.callback = callback
+    addTarget(self, action: #selector(tapped), for: event)
+  }
+  
+  @objc private func tapped() {
+    callback(self)
+  }
+}
--- try_ocr/try_ocr/ViewController.swift
+++ try_ocr/try_ocr/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import jot
+
+let buttonHeight = 100 as CGFloat
 
 class ViewController: UIViewController {
   
+  let canvas = JotViewController()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupCanvas()
+    setupMenu()
+  }
+}
+
+// MARK: - Drawer
+
+extension ViewController {
+  
+  func setupCanvas() {
     
-    OcrService.detectTexts(from: #imageLiteral(resourceName: "eng2")) { texts in
+    addChildViewController(canvas)
+    view.addSubview(canvas.view)
+    canvas.didMove(toParentViewController: self)
+    canvas.view.frame = CGRect(
+      x: 0, y: 0, width: view.viewWidth, height: view.viewHeight - buttonHeight)
+    canvas.state = .drawing
+  }
+  
+  func setupMenu() {
+    
+    // clear button
+    let clearButton = UIButton.generateButton(
+      title: "Clear",
+      frame: CGRect(
+        x: 0,
+        y: view.viewHeight - buttonHeight,
+        width: view.viewWidth / 2,
+        height: buttonHeight)) {
+          self.canvas.clearDrawing()
+    }
+    view.addSubview(clearButton)
+    
+    // extract texts
+    let extractButton = UIButton.generateButton(
+      title: "Extract",
+      frame: CGRect(
+        x: view.viewWidth / 2,
+        y: view.viewHeight - buttonHeight,
+        width: view.viewWidth / 2,
+        height: buttonHeight)) {
+        self.detectTexts()
+    }
+    view.addSubview(extractButton)
+  }
+}
+
+// MARK: - OCR
+
+extension ViewController {
+  
+  func detectTexts() {
+    guard let image = UIImage.convertToImage(view: canvas.view) else { return }
+    OcrService.detectTexts(from: image) { texts in
       if let texts = texts {
+        var result = ""
+        texts.forEach({ result += $0 })
+        UIAlertController.show(self, title: result)
         print(texts)
       } else {
-        print("No Texts")
+        UIAlertController.show(self, title: "Cannot detect text")
       }
     }
   }
 }
-
