c7a36cd175b31fe92d22f8b1d39df716b2998640
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-05-23 23:10:37 -0700

    chore: code cleanup

--- try_deco_text/try_deco_text/ViewController.swift
+++ try_deco_text/try_deco_text/ViewController.swift
@@ -23,39 +23,64 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     label = UILabel()
-    label.decoText("0111122<b>223333<em>444<b>4<em>ddddd", withColor: .yellow)
+    label.decoText(
+      "Baseball was first introduced to Japan in <b>1872<b> and <b>is<b> currently among the country's most popular participatory and spectator sports.",
+      backgroundColor: .yellow,
+      boldFont: UIFont.boldSystemFont(ofSize: 30)
+    )
     view.addSubview(label)
   }
 }
 
 extension UILabel {
   
-  func decoText(_ text: String, withColor: UIColor) {
-    
-    // devide by em tag
-    var emTexts = text.components(separatedBy: "<em>")
-    
-    // if the open/close tag is incollect
-    if emTexts.count < 3 {
-      self.text = text.replacingOccurrences(of: "<em>", with: "")
-      return;
-    }
-    
-    // remove first/last element
-    emTexts.removeFirst()
-    emTexts.removeLast()
+  func decoText(_ text: String, backgroundColor: UIColor, boldFont: UIFont) {
     
     // remove tags
-    let decoText = text.replacingOccurrences(of: "<em>", with: "")
+    let textWithoutEm = text.replacingOccurrences(of: "<em>", with: "")
+    let textWithoutB = text.replacingOccurrences(of: "<b>", with: "")
+    var textWithoutTag = text.replacingOccurrences(of: "<em>", with: "")
+    textWithoutTag = textWithoutTag.replacingOccurrences(of: "<b>", with: "")
+    
+    // devide by em tag
+    var emTexts = textWithoutB.components(separatedBy: "<em>")
+    var bTexts = textWithoutEm.components(separatedBy: "<b>")
     
     // create attribute text
-    let attrText = NSMutableAttributedString(string: decoText)
-    for emText in emTexts {
-      if let range = decoText.range(of: emText) {
-        let nsRange = decoText.nsRange(from: range)
-        attrText.addAttribute(.backgroundColor, value: withColor, range: nsRange)
+    let attrText = NSMutableAttributedString(string: textWithoutTag)
+    
+    // deco <em> texts
+    if emTexts.count > 2 {
+      
+      // remove first/last element
+      emTexts.removeFirst()
+      emTexts.removeLast()
+      
+      // deco background color
+      for emText in emTexts {
+        if let range = textWithoutTag.range(of: emText) {
+          let nsRange = textWithoutTag.nsRange(from: range)
+          attrText.addAttribute(.backgroundColor, value: backgroundColor, range: nsRange)
+        }
       }
     }
+    
+    // deco <b> tag
+    if bTexts.count > 2 {
+      
+      // remove first/last element
+      bTexts.removeFirst()
+      bTexts.removeLast()
+      
+      // deco bold color
+      for bText in bTexts {
+        if let range = textWithoutTag.range(of: bText) {
+          let nsRange = textWithoutTag.nsRange(from: range)
+          attrText.addAttribute(.font, value: boldFont, range: nsRange)
+        }
+      }
+    }
+    
     attributedText = attrText
   }
 }
