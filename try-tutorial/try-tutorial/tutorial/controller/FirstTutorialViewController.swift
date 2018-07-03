f4ba9177515fb3df901c2c4a620e8eff9a87207e
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-07-03 10:19:52 -0700

    chore: read json file

--- try-tutorial/try-tutorial/tutorial/controller/FirstTutorialViewController.swift
+++ try-tutorial/try-tutorial/tutorial/controller/FirstTutorialViewController.swift
@@ -13,5 +13,30 @@ class FirstTutorialViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
+    fetchUsersFromJson()
+  }
+  
+  fileprivate func fetchUsersFromJson() {
+    guard let path = Bundle.main.path(
+      forResource: "tutorial-users",
+      ofType: "json"
+      ) else { return }
+    
+    do {
+      let data = try Data(
+        contentsOf: URL(fileURLWithPath: path),
+        options: .mappedIfSafe
+      )
+      let jsonResult = try JSONSerialization.jsonObject(
+        with: data,
+        options: .mutableLeaves
+      )
+      if let jsonResult = jsonResult as? Dictionary<String, AnyObject>,
+        let person = jsonResult["person"] as? [Any] {
+        print(person)
+      }
+    } catch {
+      // handle error
+    }
   }
 }
