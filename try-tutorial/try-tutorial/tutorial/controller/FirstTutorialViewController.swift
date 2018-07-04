38f10bcc19e17f6768df1e7ebcecf2d6d09443d8
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-07-04 09:49:24 -0700

    chore: add search bar

--- try-tutorial/try-tutorial/tutorial/controller/FirstTutorialViewController.swift
+++ try-tutorial/try-tutorial/tutorial/controller/FirstTutorialViewController.swift
@@ -15,6 +15,21 @@ class FirstTutorialViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
+    setupNavigationItems()
+//    setupCollectionView()
+  }
+}
+
+// MARK: - NavigationItem
+
+extension FirstTutorialViewController {
+  
+  fileprivate func setupNavigationItems() {
+    let searchBar = UISearchBar()
+    searchBar.tintColor = .darkGray
+    searchBar.placeholder = "メイク・コスメ・ユーザーを検索"
+    searchBar.isUserInteractionEnabled = false
+    navigationItem.titleView = searchBar
   }
 }
 
@@ -23,6 +38,20 @@ class FirstTutorialViewController: UIViewController {
 extension FirstTutorialViewController: UICollectionViewDataSource, UICollectionViewDelegate {
   
   fileprivate func setupCollectionView() {
+    
+    // set collection view UI
+    collectionView.contentInset = UIEdgeInsetsMake(8, 0, 50, 0)
+    
+    // set layout of collectionView
+    let space = 10 as CGFloat
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.scrollDirection = .vertical
+    flowLayout.minimumLineSpacing = space
+    flowLayout.minimumInteritemSpacing = space
+    flowLayout.sectionInset = UIEdgeInsetsMake(0.0, space, 0.0, space);
+    collectionView.setCollectionViewLayout(flowLayout, animated: true, completion: nil)
+    
+    // set delegate & datasorce
     collectionView.delegate = self
     collectionView.dataSource = self
   }
@@ -35,6 +64,24 @@ extension FirstTutorialViewController: UICollectionViewDataSource, UICollectionV
   func collectionView(_ collectionView: UICollectionView,
                       cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     return UICollectionViewCell()
+  }
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      viewForSupplementaryElementOfKind kind: String,
+                      at indexPath: IndexPath) -> UICollectionReusableView {
+    
+    guard let header = collectionView.dequeueReusableSupplementaryView(
+      ofKind: UICollectionElementKindSectionHeader,
+      withReuseIdentifier: "TutorialSwitchReusableView",
+      for: indexPath) as? TutorialSwitchReusableView else {
+      fatalError("Could not find proper header")
+    }
+    
+    if kind == UICollectionElementKindSectionHeader {
+      return header
+    }
+    
+    return UICollectionReusableView()
   }
   
 }
---
+++ try-tutorial/try-tutorial/tutorial/view/TutorialSwitchReusableView.swift
@@ -0,0 +1,16 @@
+//
+//  TutorialSwitchCollectionCell.swift
+//  try-tutorial
+//
+//  Created by Wataru Maeda on 2018-07-04.
+//  Copyright © 2018 com.watarumaeda. All rights reserved.
+//
+
+import UIKit
+
+class TutorialSwitchReusableView: UICollectionReusableView {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+}
