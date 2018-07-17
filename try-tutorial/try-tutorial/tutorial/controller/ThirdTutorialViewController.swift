789833e250c172ca4c5072ede3034382a8548970
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-07-16 20:26:10 -0700

    chore: add header on the third screen

--- try-tutorial/try-tutorial/tutorial/controller/ThirdTutorialViewController.swift
+++ try-tutorial/try-tutorial/tutorial/controller/ThirdTutorialViewController.swift
@@ -10,8 +10,30 @@ import UIKit
 
 class ThirdTutorialViewController: UIViewController {
   
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
+    setupTableView()
   }
 }
+// MARK: TableView
+
+extension ThirdTutorialViewController: UITableViewDelegate, UITableViewDataSource {
+  
+  func setupTableView() {
+    tableView.dataSource = self
+    tableView.delegate = self
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+    return cell ?? UITableViewCell()
+  }
+}
+
---
+++ try-tutorial/try-tutorial/tutorial/view/TutorialRecipeDataCell.swift
@@ -0,0 +1,21 @@
+//
+//  TutorialRecipeDataCell.swift
+//  try-tutorial
+//
+//  Created by Wataru Maeda on 2018-07-16.
+//  Copyright © 2018 com.watarumaeda. All rights reserved.
+//
+
+import UIKit
+
+class TutorialRecipeDataCell: UITableViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+  
+}
