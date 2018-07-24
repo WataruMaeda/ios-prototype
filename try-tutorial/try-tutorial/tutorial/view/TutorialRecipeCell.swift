40480f94547700f7ee6d2172b1c20b1835a7854d
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-07-23 21:25:22 -0700

    chore: update thrd tutorial style

---
+++ try-tutorial/try-tutorial/tutorial/view/TutorialRecipeCell.swift
@@ -0,0 +1,63 @@
+//
+//  TutorialRecipeCell.swift
+//  try-tutorial
+//
+//  Created by Wataru Maeda on 2018-07-16.
+//  Copyright © 2018 com.watarumaeda. All rights reserved.
+//
+
+import UIKit
+
+class TutorialRecipeCell: UITableViewCell {
+  
+  @IBOutlet weak var profileImageView: UIImageView! {
+    didSet { profileImageView.circle() }
+  }
+  @IBOutlet weak var recipeImageView: UIImageView! {
+    didSet { recipeImageView.addCornerRafius() }
+  }
+  @IBOutlet weak var beforeImageView: UIImageView! {
+    didSet { beforeImageView.addCornerRafius() }
+  }
+  @IBOutlet weak var afterImageView: UIImageView! {
+    didSet { afterImageView.addCornerRafius() }
+  }
+  @IBOutlet weak var recipeImageView1: UIImageView! {
+    didSet { recipeImageView1.addCornerRafius() }
+  }
+  @IBOutlet weak var recipeImageView2: UIImageView! {
+    didSet { recipeImageView2.addCornerRafius() }
+  }
+  @IBOutlet weak var recipeImageView3: UIImageView! {
+    didSet { recipeImageView3.addCornerRafius() }
+  }
+  @IBOutlet weak var recipeImageView4: UIImageView! {
+    didSet { recipeImageView4.addCornerRafius() }
+  }
+  @IBOutlet weak var recipeImageView5: UIImageView! {
+    didSet { recipeImageView5.addCornerRafius() }
+  }
+  @IBOutlet weak var recipeImageView6: UIImageView! {
+    didSet { recipeImageView6.addCornerRafius() }
+  }
+  
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+}
+
+fileprivate extension UIView {
+  func circle() {
+    layer.cornerRadius = frame.size.width / 2
+    clipsToBounds = true
+  }
+  func addCornerRafius() {
+    layer.cornerRadius = 10
+    clipsToBounds = true
+  }
+}
