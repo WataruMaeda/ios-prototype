a9d7b24bdea0f499ef4c2331cd585bcbe27ba17f
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-08-20 12:21:13 -0700

    chore: remove default main dashboard components

---
+++ try-twitter-login-swifter/try-twitter-login-swifter/TwitterButton.swift
@@ -0,0 +1,21 @@
+//
+//  TwitterButton.swift
+//  try-twitter-login-swifter
+//
+//  Created by Wataru Maeda on 2018-08-20.
+//  Copyright © 2018 com.watarumaeda. All rights reserved.
+//
+
+import UIKit
+
+class TwitterButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
