15d6544ec788e14c503f924e70a5ceaff96a4f82
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2019-02-09 18:10:22 -0800

    init new project

---
+++ try_activity_controller/try_activity_controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  try_activity_controller
+//
+//  Created by Wataru Maeda on 2019-02-09.
+//  Copyright © 2019 com.watarumaeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
