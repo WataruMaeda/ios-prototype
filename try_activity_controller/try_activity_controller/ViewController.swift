805a1760f9b0b755015e09675ab0f1f6f4249147
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2019-02-09 18:55:04 -0800

    chore: add activityContoller to share the text

--- try_activity_controller/try_activity_controller/ViewController.swift
+++ try_activity_controller/try_activity_controller/ViewController.swift
@@ -12,9 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+        setupNaviMenu()
     }
-
-
+    
+    func setupNaviMenu() {
+        let shareButton = UIButton(type: .custom)
+        shareButton.setImage(UIImage(named: "share"), for: .normal)
+        shareButton.imageView?.contentMode = .scaleAspectFit
+        shareButton.imageEdgeInsets = UIEdgeInsets(top: 7, left: 7, bottom: 7, right: 7)
+        shareButton.addTarget(self, action: #selector(tappedShare), for: .touchUpInside)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: shareButton)
+    }
+    
+    @objc func tappedShare() {
+        var items = [Any]()
+        if let url = URL(string: "https://www.apple.com") {
+            items.append(url)
+        } else {
+            items.append("https://www.apple.com")
+        }
+        let activityController = UIActivityViewController(
+            activityItems: items,
+            applicationActivities: nil
+        )
+        present(activityController, animated: true)
+    }
 }
 
