9b4db0d849fece2244cf93e80d1644810d3d2fb8
WataruMaeda <w.maeda.jp@gmail.com>
2020-05-17 15:27:08 -0700

    chore: remove

--- try-swiftUI-weather/Pods/Target Support Files/Pods-try-swiftUI-weather-try-swiftUI-weatherUITests/Pods-try-swiftUI-weather-try-swiftUI-weatherUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_try_swiftUI_weather_try_swiftUI_weatherUITests : NSObject
-@end
-@implementation PodsDummy_Pods_try_swiftUI_weather_try_swiftUI_weatherUITests
-@end
--- try-swiftUI-weather/Pods/Target Support Files/Pods-try-swiftUI-weather/Pods-try-swiftUI-weather-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_try_swiftUI_weather : NSObject
-@end
-@implementation PodsDummy_Pods_try_swiftUI_weather
-@end
