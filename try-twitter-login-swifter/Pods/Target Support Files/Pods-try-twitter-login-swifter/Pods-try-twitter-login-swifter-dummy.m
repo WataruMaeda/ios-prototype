7b8978f39a62e2b2f8a770cb7a03ed136304a05d
WataruMaeda <w.maeda.jp@gmail.com>
2020-05-17 12:33:10 -0700

    chore: remove pods

--- try-twitter-login-swifter/Pods/Target Support Files/Pods-try-twitter-login-swifter/Pods-try-twitter-login-swifter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_try_twitter_login_swifter : NSObject
-@end
-@implementation PodsDummy_Pods_try_twitter_login_swifter
-@end
--- try_twitter_login/Pods/Target Support Files/Pods-try_twitter_login/Pods-try_twitter_login-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_try_twitter_login : NSObject
-@end
-@implementation PodsDummy_Pods_try_twitter_login
-@end
