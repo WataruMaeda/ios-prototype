65ff66f1eeffea24fa8a64fb23b3941368cd185c
WataruMaeda <w.maeda.jp@gmail.com>
2020-05-17 14:41:07 -0700

    chore: remove

---
+++ try-swiftUI-weather/try-swiftUI-weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  try-swiftUI-weather
+//
+//  Created by Wataru Maeda on 2020/05/17.
+//  Copyright © 2020 Wataru Maeda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
