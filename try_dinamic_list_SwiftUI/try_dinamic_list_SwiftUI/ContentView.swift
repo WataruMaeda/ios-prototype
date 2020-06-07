0cfff733938cd0234bf5f02887a21145baea457b
WataruMaeda <w.maeda.jp@gmail.com>
2020-06-07 11:13:39 -0700

    chore: create base project for dynamic list

---
+++ try_dinamic_list_SwiftUI/try_dinamic_list_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  try_dinamic_list_SwiftUI
+//
+//  Created by Wataru Maeda on 2020/06/07.
+//  Copyright © 2020 Wataru Maeda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
